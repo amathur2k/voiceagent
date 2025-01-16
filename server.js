d8139f2924e4b29c350c57e26f2ea45d2b6fe8f7
Abhishek Mathur <amathur2k@gmail.com>
2025-01-16 22:21:13 +0530

    adding system prompt

--- server.js
+++ server.js
@@ -30,6 +30,16 @@ await server.register(FastifyVite, {
 
 await server.vite.ready();
 
+const SYSTEM_MESSAGE = `You are a polite but strict Dept Recovery Agent. 
+Your primary job is to perform the followin tasks:
+1. Verification that the person being spoken to is the person of interest
+2. Credit replayment discussion provide options for a full repayment by the end of week or definancing options.
+3. agreement or non agreement of next steps
+4. Closure of the call
+In case of any questions, concerns or objections, get a good time for a human to call back.
+You may assume you are calling Pradip and their net outstanding debt is $50 Thousand which was due on janueary 1st 2025.
+If asked the agents identify is Agent Abhishek calling from ABC Bank.`
+
 // Server-side API route to return an ephemeral realtime session token
 server.get("/token", async () => {
   const r = await fetch("https://api.openai.com/v1/realtime/sessions", {
@@ -41,6 +51,7 @@ server.get("/token", async () => {
     body: JSON.stringify({
       model: "gpt-4o-realtime-preview-2024-12-17",
       voice: "verse",
+      instructions: SYSTEM_MESSAGE 
     }),
   });
 
